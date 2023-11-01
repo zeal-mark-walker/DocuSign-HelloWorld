221f6b10faf1c4e75e42b47e9e99738a1ca47ae4
Mark Walker <MWalker@mynexuscare.com>
2023-11-01 17:46:04 -0500

    Add project files.

---
+++ DocuSign-HelloWorld/JWTAuth.cs
@@ -0,0 +1,92 @@
+﻿// <copyright file="JWTAuth.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.CodeExamples.Authentication
+{
+    using System;
+    using System.Collections.Generic;
+    using DocuSign.CodeExamples.Common;
+    using DocuSign.eSign.Client;
+    using static DocuSign.eSign.Client.Auth.OAuth;
+    using eg_03_csharp_auth_code_grant_core.Common;
+
+    public static class JwtAuth
+    {
+        /// <summary>
+        /// Uses Json Web Token (JWT) Authentication Method to obtain the necessary information needed to make API calls.
+        /// </summary>
+        /// <returns>Auth token needed for API calls</returns>
+        public static OAuthToken AuthenticateWithJwt(string api, string clientId, string impersonatedUserId, string authServer, byte[] privateKeyBytes)
+        {
+            var docuSignClient = new DocuSignClient();
+            var apiType = Enum.Parse<ExamplesApiType>(api);
+            var scopes = new List<string>
+                {
+                    "signature",
+                    "impersonation",
+                };
+            if (apiType == ExamplesApiType.Rooms)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "dtr.rooms.read",
+                    "dtr.rooms.write",
+                    "dtr.documents.read",
+                    "dtr.documents.write",
+                    "dtr.profile.read",
+                    "dtr.profile.write",
+                    "dtr.company.read",
+                    "dtr.company.write",
+                    "room_forms",
+                });
+            }
+
+            if (apiType == ExamplesApiType.Click)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "click.manage",
+                    "click.send",
+                });
+            }
+
+            if (apiType == ExamplesApiType.Monitor)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "signature",
+                    "impersonation",
+                });
+            }
+
+            if (apiType == ExamplesApiType.Admin)
+            {
+                scopes.AddRange(new List<string>
+                {
+                    "user_read",
+                    "user_write",
+                    "account_read",
+                    "organization_read",
+                    "group_read",
+                    "permission_read",
+                    "identity_provider_read",
+                    "domain_read",
+                    "user_data_redact",
+                    "asset_group_account_read",
+                    "asset_group_account_clone_write",
+                    "asset_group_account_clone_read",
+                });
+            }
+
+            var pk = DSHelper.ReadFileContent(DSHelper.PrepareFullPrivateKeyFilePath("private_key.pem"));
+
+            return docuSignClient.RequestJWTUserToken(
+                clientId,
+                impersonatedUserId,
+                authServer,
+                pk,
+                1);
+        }
+    }
+}
